9173eb3ca1d57cf80fb80b247d77ccdd3599a0aa
麦志泉 <zhiquan911@qq.com>
2016-12-03 10:08:31 +0800

    doing BlockNodeSetting

---
+++ chance_btc_wallet/chance_btc_wallet/viewcontrollers/setting/BlockchainNodeSettingViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BlockchainNodeSettingViewController.swift
+//  chance_btc_wallet
+//
+//  Created by Chance on 2016/12/2.
+//  Copyright © 2016年 chance. All rights reserved.
+//
+
+import UIKit
+
+class BlockchainNodeSettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
